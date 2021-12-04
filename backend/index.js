1ed2c6a61a925c5bcdff4a45bdfd4795a872f385
samarendra behera <71862123+samarendra-behera@users.noreply.github.com>
2021-12-04 15:11:08 +0530

    video 45 is completed

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
+
+app.use(express.json())
+// Available Routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
 app.get('/', (req, res) => {
   res.send('Hello Papun It is Your first API Development')
 })
